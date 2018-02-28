923adc8011573998f124ba7cabd3d379b1d108c8
Nguyen Thanh Tung <tung.tung11191@gmail.com>
2018-02-28 10:54:12 +0700

    change_manifest

--- sprogroup_purchase_request/__manifest__.py
+++ sprogroup_purchase_request/__manifest__.py
@@ -12,7 +12,7 @@ Use Purchase Request module for requesting product.
     'summary': 'Create purchase request',
     'website': 'https://www.odoo.com/page/survey',
     'images': ['static/description/icon.jpg'],
-    'depends': ['mail', 'website'],
+    'depends': ['mail'],
     'data': [
         'data/sprogroup_purchase_request_data.xml',
         'security/sprogroup_purchase_request_security.xml',
