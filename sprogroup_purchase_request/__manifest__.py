7a13197d2a1bc304e5683b1d46022848fc13bc7c
Nguyen Thanh Tung <tung.tung11191@gmail.com>
2018-02-28 10:13:29 +0700

    change_manifest

--- sprogroup_purchase_request/__manifest__.py
+++ sprogroup_purchase_request/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Purchase Request',
+    'name': 'Request Purchase Product',
     'version': '1.0',
     'category': 'Purchases',
     'author':'tung.tung11191@gmail.com',
@@ -12,6 +12,7 @@ Use Purchase Request module for requesting product.
     'summary': 'Create purchase request',
     'website': 'https://www.odoo.com/page/survey',
     'depends': ['mail', 'website'],
+	'images': ['static/description/icon.jpg'],
     'data': [
         'data/sprogroup_purchase_request_data.xml',
         'security/sprogroup_purchase_request_security.xml',
@@ -23,4 +24,5 @@ Use Purchase Request module for requesting product.
     'auto_install': False,
     'application': True,
     'sequence': 105,
+    'license': 'AGPL-3',
 }
